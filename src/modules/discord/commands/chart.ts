2a9c231cdfa66234657a01435025a578bd688378
Sayak Mukhopadhyay <mukhopadhyaysayak@gmail.com>
2021-02-16 12:36:01 +0530

    Hotfix for charts not working

--- src/modules/discord/commands/chart.ts
+++ src/modules/discord/commands/chart.ts
@@ -77,8 +77,8 @@ export class Chart implements Command {
                             let requestOptions: AxiosRequestConfig = {
                                 params: {
                                     name: name,
-                                    timeMin: timenow - 10 * 24 * 60 * 60 * 1000,
-                                    timeMax: timenow,
+                                    timemin: timenow - 10 * 24 * 60 * 60 * 1000,
+                                    timemax: timenow,
                                     theme: theme
                                 }
                             };
